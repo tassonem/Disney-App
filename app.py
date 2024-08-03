fb83861fe9535d75071b5eda0d3b3cd1edd8bfb9
bra0144 <bran0144@umn.edu>
2024-08-02 21:52:36 -0500

    working on app and edit pages

--- app.py
+++ app.py
@@ -46,6 +46,27 @@ def rides():
                 cur = mysql.connection.cursor()
                 cur.execute(query, (ride_name, park_id))
                 mysql.connection.commit()
+            
+            # account for null height_restriction and null lightning_lane
+            elif height_restriction == "" and lightning_lane == "":
+                query = "INSERT INTO Rides (ride_name, park_id, ride_length) VALUES (%s, %s, %s)"
+                cur = mysql.connection.cursor()
+                cur.execute(query, (ride_name, park_id, ride_length))
+                mysql.connection.commit()
+
+            # account for null height_restriction and null ride_length
+            elif height_restriction == "" and ride_length == "":
+                query = "INSERT INTO Rides (ride_name, park_id, lightning_lane) VALUES (%s, %s, %s)"
+                cur = mysql.connection.cursor()
+                cur.execute(query, (ride_name, park_id, lightning_lane))
+                mysql.connection.commit()
+
+            # account for null lightning_lane and null ride_length
+            elif lightning_lane == "" and ride_length == "":
+                query = "INSERT INTO Rides (ride_name, park_id, height_restriction) VALUES (%s, %s, %s)"
+                cur = mysql.connection.cursor()
+                cur.execute(query, (ride_name, park_id, height_restriction))
+                mysql.connection.commit()
 
             # account for null height_restriction
             elif height_restriction == "":
@@ -75,13 +96,47 @@ def rides():
                 cur.execute(query, (ride_name, park_id, height_restriction, lightning_lane, ride_length))
                 mysql.connection.commit()
 
-            # redirect back to people page
+            # redirect back to rides page
             return redirect("/rides")
 
-    # Grab bsg_people data so we send it to our template to display
+    # Grab Rides data so we send it to our template to display
     if request.method == "GET":
-        # mySQL query to grab all the people in bsg_people
-        query = "SELECT Rides.id, ride_name, height_restriction, lightning_lane, ride_length, Parks.park_name AS park FROM Rides LEFT JOIN Parks ON park_id = Parks.park_id"
+        # mySQL query to grab all the rides in Rides
+        query = "SELECT Rides.id, ride_name, height_restriction, lightning_lane, ride_length, Parks.park_name AS park FROM Rides LEFT JOIN Parks ON park_id = Parks.park_id;"
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        data = cur.fetchall()
+
+        # mySQL query to grab park id/name data for our dropdown
+        query2 = "SELECT park_id, park_name FROM Parks;"
+        cur = mysql.connection.cursor()
+        cur.execute(query2)
+        parks_data = cur.fetchall()
+
+        # render edit_rides page passing our query data and parks data to the edit_rides template
+        return render_template("rides.j2", data=data, parks=parks_data)
+
+# route for delete functionality, deleting a ride from Rides,
+# we want to pass the 'id' value of that ride on button click (see HTML) via the route
+@app.route("/delete_ride/<int:id>")
+def delete_ride(id):
+    # mySQL query to delete the ride with our passed id
+    query = "DELETE FROM Rides WHERE id = '%s';"
+    cur = mysql.connection.cursor()
+    cur.execute(query, (id,))
+    mysql.connection.commit()
+
+    # redirect back to people page
+    return redirect("/rides")
+
+
+# route for edit functionality, updating the attributes of a ride in Rides
+# similar to our delete route, we want to the pass the 'id' value of that ride on button click (see HTML) via the route
+@app.route("/edit_ride/<int:id>", methods=["POST", "GET"])
+def edit_ride(id):
+    if request.method == "GET":
+        # mySQL query to grab the info of the person with our passed id
+        query = "SELECT * FROM Rides WHERE id = %s" % (id)
         cur = mysql.connection.cursor()
         cur.execute(query)
         data = cur.fetchall()
@@ -90,87 +145,88 @@ def rides():
         query2 = "SELECT park_id, park_name FROM Parks"
         cur = mysql.connection.cursor()
         cur.execute(query2)
-        homeworld_data = cur.fetchall()
+        parks_data = cur.fetchall()
 
-        # render edit_people page passing our query data and homeworld data to the edit_people template
-        return render_template("people.j2", data=data, homeworlds=homeworld_data)
-
-# route for delete functionality, deleting a person from bsg_people,
-# we want to pass the 'id' value of that person on button click (see HTML) via the route
-@app.route("/delete_people/<int:id>")
-def delete_people(id):
-    # mySQL query to delete the person with our passed id
-    query = "DELETE FROM bsg_people WHERE id = '%s';"
-    cur = mysql.connection.cursor()
-    cur.execute(query, (id,))
-    mysql.connection.commit()
-
-    # redirect back to people page
-    return redirect("/people")
-
-
-# route for edit functionality, updating the attributes of a person in bsg_people
-# similar to our delete route, we want to the pass the 'id' value of that person on button click (see HTML) via the route
-@app.route("/edit_people/<int:id>", methods=["POST", "GET"])
-def edit_people(id):
-    if request.method == "GET":
-        # mySQL query to grab the info of the person with our passed id
-        query = "SELECT * FROM bsg_people WHERE id = %s" % (id)
-        cur = mysql.connection.cursor()
-        cur.execute(query)
-        data = cur.fetchall()
-
-        # mySQL query to grab planet id/name data for our dropdown
-        query2 = "SELECT id, name FROM bsg_planets"
-        cur = mysql.connection.cursor()
-        cur.execute(query2)
-        homeworld_data = cur.fetchall()
-
-        # render edit_people page passing our query data and homeworld data to the edit_people template
-        return render_template("edit_people.j2", data=data, homeworlds=homeworld_data)
+        # render edit_rides page passing our query data and parks data to the edit_ride template
+        return render_template("edit_rides.j2", data=data, parks=parks_data)
 
     # meat and potatoes of our update functionality
     if request.method == "POST":
-        # fire off if user clicks the 'Edit Person' button
-        if request.form.get("Edit_Person"):
+        # fire off if user clicks the 'Edit Ride' button
+        if request.form.get("Edit_Ride"):
             # grab user form inputs
-            id = request.form["personID"]
-            fname = request.form["fname"]
-            lname = request.form["lname"]
-            homeworld = request.form["homeworld"]
-            age = request.form["age"]
+            ride_id = request.form["ride_id"]
+            ride_name = request.form["ride_name"]
+            height_restriction = request.form["height_restriction"]
+            lightning_lane = request.form["lightning_lane"]
+            park_id = request.form["park_id"]
+            ride_length = request.form["ride_length"]
 
-            # account for null age AND homeworld
-            if (age == "" or age == "None") and homeworld == "0":
-                # mySQL query to update the attributes of person with our passed id value
-                query = "UPDATE bsg_people SET bsg_people.fname = %s, bsg_people.lname = %s, bsg_people.homeworld = NULL, bsg_people.age = NULL WHERE bsg_people.id = %s"
+            # account for null height_restriction AND lightning_lane AND ride_length
+            if height_restriction == "" and lightning_lane == "" and ride_length == "":
+                # mySQL query to update the attributes of ride with our passed id value
+                query = "UPDATE Rides SET Rides.ride_name = %s, Rides.park_id = %s, Rides.height_restriction = NULL, Rides.lightning_lane = NULL, Rides.ride_length = NULL WHERE Rides.ride_id = %s"
                 cur = mysql.connection.cursor()
-                cur.execute(query, (fname, lname, id))
+                cur.execute(query, (ride_name, park_id, ride_id))
                 mysql.connection.commit()
 
-            # account for null homeworld
-            elif homeworld == "0":
-                query = "UPDATE bsg_people SET bsg_people.fname = %s, bsg_people.lname = %s, bsg_people.homeworld = NULL, bsg_people.age = %s WHERE bsg_people.id = %s"
+            # account for null height_restriction AND lightning_lane
+            elif height_restriction == "" and lightning_lane == "":
+                # mySQL query to update the attributes of ride with our passed id value
+                query = "UPDATE Rides SET Rides.ride_name = %s, Rides.park_id = %s, Rides.ride_length = %s, Rides.height_restriction = NULL, Rides.lightning_lane = NULL WHERE Rides.ride_id = %s"
                 cur = mysql.connection.cursor()
-                cur.execute(query, (fname, lname, age, id))
+                cur.execute(query, (ride_name, park_id, ride_length, ride_id))
                 mysql.connection.commit()
 
-            # account for null age
-            elif age == "" or age == "None":
-                query = "UPDATE bsg_people SET bsg_people.fname = %s, bsg_people.lname = %s, bsg_people.homeworld = %s, bsg_people.age = NULL WHERE bsg_people.id = %s"
+            # account for null height_restriction AND ride_length
+            elif height_restriction == "" and ride_length == "":
+                # mySQL query to update the attributes of ride with our passed id value
+                query = "UPDATE Rides SET Rides.ride_name = %s, Rides.park_id = %s, Rides.lightning_lane = %s, Rides.height_restriction = NULL, Rides.ride_length = NULL WHERE Rides.ride_id = %s"
                 cur = mysql.connection.cursor()
-                cur.execute(query, (fname, lname, homeworld, id))
+                cur.execute(query, (ride_name, park_id, lightning_lane, ride_id))
+                mysql.connection.commit()
+
+            # account for null ride_length AND lightning_lane
+            elif lightning_lane == "" and ride_length == "":
+                # mySQL query to update the attributes of ride with our passed id value
+                query = "UPDATE Rides SET Rides.ride_name = %s, Rides.park_id = %s, Rides.height_restriction = %s, Rides.lightning_lane = NULL, Rides.ride_length = NULL WHERE Rides.ride_id = %s"
+                cur = mysql.connection.cursor()
+                cur.execute(query, (ride_name, park_id, height_restriction, ride_id))
+                mysql.connection.commit()
+            
+            # account for null lightning_lane
+            elif lightning_lane == "" :
+                # mySQL query to update the attributes of ride with our passed id value
+                query = "UPDATE Rides SET Rides.ride_name = %s, Rides.park_id = %s, Rides.height_restriction = %s, Rides.ride_length = %s, Rides.lightning_lane = NULL WHERE Rides.ride_id = %s"
+                cur = mysql.connection.cursor()
+                cur.execute(query, (ride_name, park_id, height_restriction, ride_length, ride_id))
+                mysql.connection.commit()
+
+            # account for null height_restriction
+            elif height_restriction == "" :
+                # mySQL query to update the attributes of ride with our passed id value
+                query = "UPDATE Rides SET Rides.ride_name = %s, Rides.park_id = %s, Rides.lightning_lane = %s, Rides.ride_length = %s, Rides.height_restriction = NULL WHERE Rides.ride_id = %s"
+                cur = mysql.connection.cursor()
+                cur.execute(query, (ride_name, park_id, lightning_lane, ride_length, ride_id))
+                mysql.connection.commit()
+
+            # account for null ride_length
+            elif ride_length == "" :
+                # mySQL query to update the attributes of ride with our passed id value
+                query = "UPDATE Rides SET Rides.ride_name = %s, Rides.park_id = %s, Rides.lightning_lane = %s, Rides.height_restriction = %s, Rides.ride_length = NULL WHERE Rides.ride_id = %s"
+                cur = mysql.connection.cursor()
+                cur.execute(query, (ride_name, park_id, lightning_lane, height_restriction, ride_id))
                 mysql.connection.commit()
 
             # no null inputs
             else:
-                query = "UPDATE bsg_people SET bsg_people.fname = %s, bsg_people.lname = %s, bsg_people.homeworld = %s, bsg_people.age = %s WHERE bsg_people.id = %s"
+                query = "UPDATE Rides SET Rides.ride_name= %s, Rides.park_id = %s, Rides.lightning_lane = %s, Rides.height_restriction = %s Rides.ride_length = %s WHERE Rides.ride_id = %s"
                 cur = mysql.connection.cursor()
-                cur.execute(query, (fname, lname, homeworld, age, id))
+                cur.execute(query, (ride_name, park_id, lightning_lane, height_restriction, ride_length, ride_id))
                 mysql.connection.commit()
 
-            # redirect back to people page after we execute the update query
-            return redirect("/people")
+            # redirect back to rides page after we execute the update query
+            return redirect("/rides")
 
 
 
@@ -180,7 +236,3 @@ if __name__ == "__main__":
 
     #Start the app on port 3000, it will be different once hosted
     app.run(port=8675, debug=True)
-
-# if __name__ == "__main__":
-#     port = int(os.environ.get('PORT', 9112))
-#     app.run(port=port, debug=True)
